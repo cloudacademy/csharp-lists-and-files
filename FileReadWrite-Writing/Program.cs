66779b0e5cef0c27261c4d4116de1bdc5dd0ebac
hallamw <58642499+hallamw@users.noreply.github.com>
2021-06-25 10:44:52 +1200

    Add files via upload

---
+++ FileReadWrite-Writing/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace FileReadWrite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Uncomment the line below when running in Windows
+            SolarSystem solarSystem = new SolarSystem($"{Environment.CurrentDirectory}\\Data\\planets.txt");
+
+            // Uncomment the line below when running in Linux/MacOS
+            //SolarSystem solarSystem = new SolarSystem($"{Environment.CurrentDirectory}/Data/planets.txt");
+            
+            Console.Write(solarSystem.ToString());
+            Console.WriteLine();
+
+            Console.Write("Update planet's distance from sun. Enter planet name: ");
+            string planetName = Console.ReadLine();
+            Console.Write("Enter new distance from sun: ");
+            string distance = Console.ReadLine();
+
+            int km = 0;
+            bool isInt = int.TryParse(distance, out km);
+
+            if (isInt)
+            {
+                if (solarSystem.UpdatePlanet(planetName, km))
+                {
+                    solarSystem.Save();
+                    Console.WriteLine();
+                    Console.Write(solarSystem.ToString());               
+                }
+                else
+                {
+                    Console.WriteLine($"{planetName} is not in the solar system");                  
+                }
+            }
+            else 
+            {
+                Console.WriteLine($"{distance} is not an integer");
+            }
+           
+        }
+    }
+}
